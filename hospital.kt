c346b092d590587325794ea93157c76bba3e9248
leellyb <lilobee20@gmail.com>
2021-04-09 13:09:34 +0300

    quiz

---
+++ hospital.kt
@@ -0,0 +1,36 @@
+//open class hospital (name: String, head: String, id: Int) {
+//    init {
+//        println("The name is: ${name}")
+//        println("The head is: ${head}")
+//        println("The Id is: ${id}")
+//    }
+//}
+//class dental(name: String, head: String, id: Int): hospital(name, head, id) {
+//    fun rootcanal() {
+//        println("The department specializes with root canal")
+//    }
+//
+//}
+//class general(name: String, head: String, id: Int): hospital(name, head, id) {
+//    fun emergency() {
+//        println("The department specializes with emergencies ")
+//    }
+//}
+//class surgery(name: String, head: String, id: Int): hospital(name, head, id) {
+//    fun neurology() {
+//        println("The department specializes with brain surgery")
+//    }
+//}
+
+//quiz 2
+
+//fun main() {
+//    val depX= dental("white smile","DR Nelson", 2345 )
+//    depX.rootcanal()
+//    val depY= general("ER","DR Bloom", 2230 )
+//    depY.emergency()
+//    val depZ= surgery("Neuro","DR Sharpe", 2040 )
+//    depZ.neurology()
+//
+//}
+
